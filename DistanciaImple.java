20439a33e835fd85b704621c1f727c4233d13b4d
José Victor <67513012+Zvcs@users.noreply.github.com>
2021-10-16 12:07:12 -0300

    Update DistanciaImple.java

--- DistanciaImple.java
+++ DistanciaImple.java
@@ -5,7 +5,11 @@ public class DistanciaImple extends UnicastRemoteObject implements Distancia{
     protected DistanciaImple () throws RemoteException {
         super();
     }
-    public long add(long a, long b) throws RemoteException{
-        return a+b;
+    public double dist(double x1, double y1, double x2, double y2) throws RemoteException{
+    double distance, x, y;
+    x = x2 - x1;
+    y = y2 - y1;
+    distance = Math.sqrt(x*x + y*y);
+    return  distance;       
     }
 }

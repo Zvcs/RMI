d422bc5f1682cf17b0f32adcb7ce5cd2c371550f
José Victor <67513012+Zvcs@users.noreply.github.com>
2021-10-16 12:06:43 -0300

    Update Distancia.java

--- Distancia.java
+++ Distancia.java
@@ -1,7 +1,7 @@
 import java.rmi.Remote;
 import java.rmi.RemoteException;
-import java.math;
+
 
 public interface Distancia extends Remote{
-    public long add(long a, long b) throws RemoteException;
+    double dist(double x1, double y1, double x2, double y2) throws RemoteException;
 }

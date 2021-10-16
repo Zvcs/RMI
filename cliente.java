b88d84b53de855dbca41eb1806febc80b1de4a4f
José Victor <67513012+Zvcs@users.noreply.github.com>
2021-10-16 12:08:05 -0300

    Update cliente.java

--- cliente.java
+++ cliente.java
@@ -1,11 +1,20 @@
 import java.rmi.Naming;
+import java.util.Scanner;
 
-public class calculardist{
+public class cliente{
     
-    public static void main(String[] args){
+    public static void main(String[] args) throws Exception {
+    double x1, x2, y1, y2;
+    Scanner scan = new Scanner(System.in);
+    System.out.print("Digite as coordenadas do primeiro ponto");
+    x1 = scan.nextInt();
+    y1 = scan.nextInt();
+    System.out.print("Digite as coordenadas do segundo ponto");
+    x2 = scan.nextInt();
+    y2 = scan.nextInt();
         try{
-            Distancia d = (Distancia) Naming.lookup(String name = "//192.168.1.106:24/DistanciaServer");
-            System.out.println("Distanicia : "+d);
+            DistanciaImple d = (DistanciaImple) Naming.lookup("rmi://localhost:1099/dist");
+            System.out.println("A distância entre dois pontos é de: " + d.dist(x1, y1, x2, y2));
         }
         catch (Exception e){
             System.out.println(e);

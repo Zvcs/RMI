d9a9dee1c5b7b5c9edebc21963905cc654c7946d
José Victor <67513012+Zvcs@users.noreply.github.com>
2021-10-19 19:13:21 -0300

    Delete Distancia.java

--- Distancia.java
+++
@@ -1,7 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-
-public interface Distancia extends Remote{
-    public double dist(double x1, double y1, double x2, double y2) throws RemoteException;
-}
